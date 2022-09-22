25eb799d2507bf1b372d7b1decf5e58190b292d9
saifkandil <saifsalaheldeen2002@gmail.com>
2022-09-22 18:23:50 +0200

    step 5 is done

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,10 +1,13 @@
 package com.udacity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.DecelerateInterpolator
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,21 +16,98 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new -> }
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var value = 0f
 
+    private var rectF = RectF(
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f
+    )
+
+    private fun RectF.computeLTRBLoadingState() {
+        left = widthSize * (8.0f / 10.0f)
+        top = heightSize * (1.0f / 10.0f)
+        right = left + 80.0f
+        bottom = top + 80.0f
     }
 
+    private fun RectF.loadingRect() {
+        left = 0.0f
+        top = 0.0f
+        right = value * widthSize.toFloat()
+        bottom = heightSize.toFloat()
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.BLACK
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create( "", Typeface.BOLD)
+    }
 
     init {
-
+        isClickable = true
     }
 
+    override fun performClick(): Boolean {
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        valueAnimator.addUpdateListener {
+            value = it.animatedValue as Float
+            invalidate()
+        }
+        valueAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                isEnabled = false
+                buttonState = ButtonState.Loading
+
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+                isEnabled = true
+                buttonState = ButtonState.Completed
+                value = 0f
+            }
+        })
+
+        valueAnimator.interpolator = DecelerateInterpolator()
+        valueAnimator.duration = 1000
+        valueAnimator.start()
+        return super.performClick()
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        canvas?.drawColor(
+            context.getColor(R.color.colorPrimary)
+        )
+        paint.color = context.getColor(R.color.colorPrimaryDark)
+        rectF.loadingRect()
+        canvas?.drawRect(
+            rectF,
+            paint
+        )
+        paint.color = context.getColor(R.color.colorAccent)
+        rectF.computeLTRBLoadingState()
+        canvas?.drawArc(
+            rectF, 0f, -1.0f * value * 360f, true, paint
+        )
+        paint.color = Color.BLACK
+        drawText(canvas)
+    }
 
+    private fun drawText(canvas: Canvas?) {
+        canvas?.drawText(
+            when (buttonState){
+                ButtonState.Loading -> context.getString(R.string.button_loading)
+                else -> context.getString(R.string.button_initial)
+            },
+            (canvas.width / 2.0f ),
+            (canvas.height / 1.6f ),
+            paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,11 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +21,9 @@ import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    var radioGroup: RadioGroup? = null
+    lateinit var radioButton: RadioButton
+    var selectedOption: Int = -1
 
     private var downloadID: Long = 0
 
@@ -31,7 +39,19 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            radioGroup = findViewById(R.id.radio_list)
+            selectedOption = radioGroup!!.checkedRadioButtonId
+
+            when (selectedOption) {
+                -1 -> Toast.makeText(baseContext, R.string.error_state, Toast.LENGTH_SHORT).show()
+                else -> {
+                    radioButton = findViewById(selectedOption)
+                    try {
+                        download(selectedOption)
+                    }
+                    catch (e: Exception) {}
+                }
+            }
         }
     }
 
@@ -41,9 +61,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
+    private fun download(selectedOption: Int) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(
+                when (selectedOption) {
+                    // todo fix here
+                    0 -> Uri.parse(URL)
+                    1 -> Uri.parse(URL2)
+                    else -> Uri.parse(URL3)
+                }
+            )
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -57,7 +84,11 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val URL =
+            "https://github.com/bumptech/glide/archive/master.zip"
+        private const val URL2 =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL3 =
+            "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
